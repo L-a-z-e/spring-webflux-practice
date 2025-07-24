03920073fd90f4a7dd39fe3b8d5de6952a2c1a29
L-a-z-e <yysi8771@gmail.com>
2025-07-24 23:20:10 +0900

    spring-webflux-practice first-commit

---
+++ src/main/java/com/laze/springwebfluxpractice/SpringWebfluxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.laze.springwebfluxpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laze/springwebfluxpractice/SpringWebfluxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laze.springwebfluxpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
